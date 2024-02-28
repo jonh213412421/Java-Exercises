3dd6781c7b6170b59fb1648159831b7cf17e781e
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-02-27 21:07:01 -0300

    Create Client.java

---
+++ SimpleRegister/Client.java
@@ -0,0 +1,19 @@
+//create client struct
+public class Client {
+	public String name;
+	public String phone;
+	public String address;
+	public int age;
+	public String gender;
+	public String coments;
+	
+	public Client (String name, String phone, String address, int age, String gender, String coments) {
+		this.name = name;
+		this.phone = phone;
+		this.address = address;
+		this.age = age;
+		this.gender = gender;
+		this.coments = coments;
+	}
+}
+

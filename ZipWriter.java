640c9d3fad7dc166b5cd9d756afaa85aefc10a19
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-03-30 19:27:08 -0300

    Create ZipWriter.java

---
+++ ZipWriter.java
@@ -0,0 +1,39 @@
+package writer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipWriter {
+	public static void main(String[] args) {
+		String pathfile = "path.ext"; //put path here
+		String outputfile = System.getProperty("user.home") + "/Desktop/" + "zip.zip"; //output in desktop with name zip.zip
+		
+		try {
+			FileOutputStream fos = new FileOutputStream(outputfile); //create fileoutputstream and point it to Desktop
+			ZipOutputStream zos = new ZipOutputStream(fos); //wrap fileoutputstream with zipoutputstream
+			File filetoZip = new File(pathfile); //create File obj with path
+			ZipEntry ze = new ZipEntry(filetoZip.getName()); //get path to zip entry
+			zos.putNextEntry(ze); //put entry in zos
+			FileInputStream fis = new FileInputStream(pathfile);
+			
+			byte[] buffer = new byte[1024];
+			int length;
+			
+			while ((length = fis.read(buffer)) > 0) {
+				zos.write(buffer, 0, length); //loop to write file
+			}
+			//close processes
+			fis.close();
+			zos.closeEntry();
+			zos.close();
+			
+			System.out.println("file has been zipped.");
+		} catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
